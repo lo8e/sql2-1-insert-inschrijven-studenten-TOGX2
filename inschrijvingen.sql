dfd0fc94aaeaaa187a93c787845caf55d5d6244d
TOGX2 <47853544+TOGX2@users.noreply.github.com>
2019-05-08 09:47:00 +0200

    Added the insert into code

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -3,3 +3,5 @@ INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES
  ('lo9e-amo1','bkr02', 2019, 'amo'),
  ('lo9e-amo2','vlt03', 2019, 'amo'), 
  ('lo9e-amo3','rs001', 2019, 'amo');
+ INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
+ 
