98cd2bfa3130a292fc66308ce26c3e3f29a280aa
TOGX2 <47853544+TOGX2@users.noreply.github.com>
2019-05-08 09:48:02 +0200

    Added first student

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -4,4 +4,4 @@ INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES
  ('lo9e-amo2','vlt03', 2019, 'amo'), 
  ('lo9e-amo3','rs001', 2019, 'amo');
  INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
- 
+ (90001,'ChooChoo','','Thomas','1984-09-04','4269 FY','Ho', 69, 1.86, 31-05-12568),
